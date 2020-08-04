ab78018fb0c435a3887d6a2af12cf8cd7eab76fc
yasuki wada <gaudi50891@gmail.com>
2020-08-05 07:08:57 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,135 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { memo } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from "constants";
+
+const name = "hello";
+
+let nameChange = "hello";
+nameChange = "hello2";
+
+let username: string = "Hello";
+
+let dummyNum: number = 2;
+let bool: boolean = true;
+
+let array1 = [true, false, true];
+let array2 = [0, 1, "hello"];
+
+interface NAME {
+  first: string;
+  last: string | null;
+}
+
+let nameObj: NAME = {
+  first: "Yamada",
+  last: null,
+};
+
+const func1 = (x: number, y: number): number => {
+  return x + y;
+};
+
+//Intersection Types
+type PROFILE = {
+  age: number;
+  city: string;
+};
+
+type LOGIN = {
+  username: string;
+  password: string;
+};
+
+type USER = PROFILE & LOGIN;
+
+const userA: USER = {
+  age: 1,
+  city: "himeji",
+  username: "yamada tarou",
+  password: "himejitarou",
+};
+
+//Union Types
+let value: boolean | number;
+value = true;
+value = 2;
+
+let arrayUni: (number | string)[];
+arrayUni = [2, "2"];
+
+let company: "Facebook" | "Google" | "Amazon";
+company = "Amazon";
+
+let memory: 256 | 512 | 1024;
+memory = 256;
+
+//typeof
+let msg: string = "Hi";
+let msg2: typeof msg;
+msg2 = "msg2";
+
+let animal = { cat: "small cat" };
+let newAnimal: typeof animal;
+newAnimal = {
+  cat: "big cat",
+};
+
+// keyof
+type KEYS = {
+  primary: string;
+  secondary: string;
+};
+
+let key: keyof KEYS;
+key = "primary";
+
+// typeof + keyof
+const SPORTS = {
+  soccer: "Soccer",
+  baseball: "Baseball",
+};
+
+let keySport: keyof typeof SPORTS;
+keySport = "soccer";
+
+// enum
+enum OS {
+  Windows,
+  Mac,
+  Linux,
+}
+
+interface PC {
+  id: number;
+  OSType: OS;
+}
+
+const PC1: PC = {
+  id: 1,
+  OSType: OS.Windows,
+};
+
+const PC2: PC = {
+  id: 2,
+  OSType: OS.Mac,
+};
+
+// 型の互換性
+
+const comp1 = "test";
+let comp2: string = comp1;
+
+let funccomp1 = (x: number) => {};
+let funccomp2 = (x: string) => {};
+
+// NG funccomp1 = funccomp2
+// NG funccomp2 = funccomp1
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
